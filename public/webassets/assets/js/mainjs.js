55ebcbe1f770c9f56554da9cc44248c2b94d3958
Ah-Sy-Yf <a.sayed.xc@gmail.com>
2023-08-22 19:12:28 +0300

    minify css and js

--- public/webassets/assets/js/mainjs.js
+++ public/webassets/assets/js/mainjs.js
@@ -243,54 +243,56 @@
     //   }
 
     //Progress Bar / Levels
-    //   if ($(".progress-levels .progress-box .bar-fill").length) {
-    //     $(".progress-box .bar-fill").each(
-    //       function () {
-    //         $(".progress-box .bar-fill").appear(function () {
-    //           var progressWidth = $(this).attr("data-percent");
-    //           $(this).css("width", progressWidth + "%");
-    //         });
-    //       },
-    //       {
-    //         accY: 0,
-    //       }
-    //     );
-    //   }
+    if ($(".progress-levels .progress-box .bar-fill").length) {
+        $(".progress-box .bar-fill").each(
+            function () {
+                $(".progress-box .bar-fill").appear(function () {
+                    var progressWidth = $(this).attr("data-percent");
+                    $(this).css("width", progressWidth + "%");
+                });
+            },
+            {
+                accY: 0,
+            }
+        );
+    }
 
-    //   //Fact Counter + Text Count
-    //   if ($(".count-box").length) {
-    //     $(".count-box").appear(
-    //       function () {
-    //         var $t = $(this),
-    //           n = $t.find(".count-text").attr("data-stop"),
-    //           r = parseInt($t.find(".count-text").attr("data-speed"), 10);
+    //Fact Counter + Text Count
+    if ($(".count-box").length) {
+        $(".count-box").appear(
+            function () {
+                var $t = $(this),
+                    n = $t.find(".count-text").attr("data-stop"),
+                    r = parseInt($t.find(".count-text").attr("data-speed"), 10);
 
-    //         if (!$t.hasClass("counted")) {
-    //           $t.addClass("counted");
-    //           $({
-    //             countNum: $t.find(".count-text").text(),
-    //           }).animate(
-    //             {
-    //               countNum: n,
-    //             },
-    //             {
-    //               duration: r,
-    //               easing: "linear",
-    //               step: function () {
-    //                 $t.find(".count-text").text(Math.floor(this.countNum));
-    //               },
-    //               complete: function () {
-    //                 $t.find(".count-text").text(this.countNum);
-    //               },
-    //             }
-    //           );
-    //         }
-    //       },
-    //       {
-    //         accY: 0,
-    //       }
-    //     );
-    //   }
+                if (!$t.hasClass("counted")) {
+                    $t.addClass("counted");
+                    $({
+                        countNum: $t.find(".count-text").text(),
+                    }).animate(
+                        {
+                            countNum: n,
+                        },
+                        {
+                            duration: r,
+                            easing: "linear",
+                            step: function () {
+                                $t.find(".count-text").text(
+                                    Math.floor(this.countNum)
+                                );
+                            },
+                            complete: function () {
+                                $t.find(".count-text").text(this.countNum);
+                            },
+                        }
+                    );
+                }
+            },
+            {
+                accY: 0,
+            }
+        );
+    }
 
     //   // Accrodion
     //   if ($(".accrodion-grp").length) {
