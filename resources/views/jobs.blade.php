07f87f8e8bb4aa1e753a649bdb8b257d92ef5eff
sabreenMohammed107 <sabreenm312@gmail.com>
2023-08-23 15:43:40 +0200

    test

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -43,10 +43,10 @@
 <link rel="stylesheet" href="{{asset('webassets/assets/vendors/ogenix-icons/style.css')}}">
 <link rel="stylesheet" href="{{asset('webassets/assets/vendors/tiny-slider/tiny-slider.min.css')}}" />
 <link rel="stylesheet" href="{{asset('webassets/assets/vendors/reey-font/stylesheet.css')}}" />
-<link rel="stylesheet" href="{{asset('webassets/assets/vendors/owl-carousel/owl.carousel.min.css')}}" />
-<link rel="stylesheet" href="{{asset('webassets/assets/vendors/owl-carousel/owl.theme.default.min.css')}}" />
+{{-- <link rel="stylesheet" href="{{asset('webassets/assets/vendors/owl-carousel/owl.carousel.min.css')}}" /> --}}
+{{-- <link rel="stylesheet" href="{{asset('webassets/assets/vendors/owl-carousel/owl.theme.default.min.css')}}" /> --}}
 <link rel="stylesheet" href="{{asset('webassets/assets/vendors/bxslider/jquery.bxslider.css')}}" />
-<link rel="stylesheet" href="{{asset('webassets/assets/vendors/bootstrap-select/css/bootstrap-select.min.css')}}" />
+{{-- <link rel="stylesheet" href="{{asset('webassets/assets/vendors/bootstrap-select/css/bootstrap-select.min.css')}}" /> --}}
 <link rel="stylesheet" href="{{asset('webassets/assets/vendors/vegas/vegas.min.css')}}" />
 <link rel="stylesheet" href="{{asset('webassets/assets/vendors/jquery-ui/jquery-ui.css')}}" />
 <link rel="stylesheet" href="{{asset('webassets/assets/vendors/timepicker/timePicker.css')}}" />
@@ -145,12 +145,12 @@
 
 <script async  src="{{asset('webassets/assets/vendors/tiny-slider/tiny-slider.min.js')}}"></script>
 <script async  src="{{asset('webassets/assets/vendors/wnumb/wNumb.min.js')}}"></script>
-<script async  src="{{asset('webassets/assets/vendors/wow/wow.js')}}"></script>
+{{-- <script async  src="{{asset('webassets/assets/vendors/wow/wow.js')}}"></script> --}}
 <script async  src="{{asset('webassets/assets/vendors/isotope/isotope.js')}}"></script>
 <script async  src="{{asset('webassets/assets/vendors/countdown/jquery.countdown.min.js')}}"></script>
-<script async  src="{{asset('webassets/assets/vendors/owl-carousel/owl.carousel.min.js')}}"></script>
+{{-- <script async  src="{{asset('webassets/assets/vendors/owl-carousel/owl.carousel.min.js')}}"></script> --}}
 <script async  src="{{asset('webassets/assets/vendors/bxslider/jquery.bxslider.min.js')}}"></script>
-<script async  src="{{asset('webassets/assets/vendors/bootstrap-select/js/bootstrap-select.min.js')}}"></script>
+{{-- <script async  src="{{asset('webassets/assets/vendors/bootstrap-select/js/bootstrap-select.min.js')}}"></script> --}}
 <script async  src="{{asset('webassets/assets/vendors/vegas/vegas.min.js')}}"></script>
 <script async  src="{{asset('webassets/assets/vendors/jquery-ui/jquery-ui.js')}}"></script>
 <script async  src="{{asset('webassets/assets/vendors/timepicker/timePicker.js')}}"></script>
