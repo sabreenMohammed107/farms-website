6384124ac21160716a636c60e75d7cf3b31b188a
Ah-Sy-Yf <a.sayed.xc@gmail.com>
2023-08-28 01:05:17 +0300

    enhance links for every page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,62 +1,67 @@
 <!DOCTYPE html>
-    <html lang="{{ config('app.locale') }}">
+<html lang="{{ config('app.locale') }}">
 
-    <head>
-        <title> Gamal Gomaa Farms - مزارع جمال جمعة </title>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- facebook meta tags -->
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Gamal Gomaa Farms - مزارع جمال جمعة" />
-        <meta property="og:description"
-            content="Gamal Gomaa Farms - مزارع جمال جمعة
+<head>
+    <title> Gamal Gomaa Farms - مزارع جمال جمعة </title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- facebook meta tags -->
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Gamal Gomaa Farms - مزارع جمال جمعة" />
+    <meta property="og:description"
+        content="Gamal Gomaa Farms - مزارع جمال جمعة
             تأسست الشركة بهدف إنتاج وتوريد منتجات البساتين الطازجة ذات الجودة الممتازة والآمنة والصحية والتي تتسم بالاستدامة لعملائنا طوال السنة بأسعار تتسم بالتنافسية العالية.
 
     تأسست الشركة بهدف استجابة منا لشغفنا بالتجديد المستمر واحترامنا للبيئة، نراهن على التكنولوجيات الحديثة التي تسهل أن نضع في السوق منتجات تتسم بالجودة الممتازة وتمت تعبئتها تحت إشراف صارم ويتم توريدها" />
-            <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
-            <link rel="canonical" href="{{ url()->current() }}" />
+    <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
+    <link rel="canonical" href="{{ url()->current() }}" />
 
-            <meta property="og:url" content="http://farms-website.seniorsteps.net/" />
-        <meta property="og:image" content= href="assets/images/resources/232659713_204434118362200_47229516744215480_n_1-1-removebg-preview.png" />
-        <meta property="og:image:alt" content="Gamal Gomaa Farms - مزارع جمال جمعة" />
-        <meta property="og:site_name" content="farms-website.com" />
-        <!-- twitter meta tags -->
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="farms-website.com">
-        <meta name="twitter:creator" content="farms-website.com">
-        <meta name="twitter:title" content="Gamal Gomaa Farms - مزارع جمال جمعة" />
-        <meta name="twitter:description"
+    <meta property="og:url" content="http://farms-website.seniorsteps.net/" />
+    <meta property="og:image"
+        content=href="assets/images/resources/232659713_204434118362200_47229516744215480_n_1-1-removebg-preview.png" />
+    <meta property="og:image:alt" content="Gamal Gomaa Farms - مزارع جمال جمعة" />
+    <meta property="og:site_name" content="farms-website.com" />
+    <!-- twitter meta tags -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="farms-website.com">
+    <meta name="twitter:creator" content="farms-website.com">
+    <meta name="twitter:title" content="Gamal Gomaa Farms - مزارع جمال جمعة" />
+    <meta name="twitter:description"
         content="Gamal Gomaa Farms - مزارع جمال جمعة
         تأسست الشركة بهدف إنتاج وتوريد منتجات البساتين الطازجة ذات الجودة الممتازة والآمنة والصحية والتي تتسم بالاستدامة لعملائنا طوال السنة بأسعار تتسم بالتنافسية العالية.
 
 تأسست الشركة بهدف استجابة منا لشغفنا بالتجديد المستمر واحترامنا للبيئة، نراهن على التكنولوجيات الحديثة التي تسهل أن نضع في السوق منتجات تتسم بالجودة الممتازة وتمت تعبئتها تحت إشراف صارم ويتم توريدها" />
-        <meta name="twitter:url" content="http://farms-website.seniorsteps.net/" />
-        <meta name="twitter:image" content= href="assets/images/resources/232659713_204434118362200_47229516744215480_n_1-1-removebg-preview.png" />
-        <meta property="twitter:image:alt" content="Gamal Gomaa Farms - مزارع جمال جمعة" />
-        <!-- general meta tags  -->
-        <meta name="canonical_tag" content="http://farms-website.seniorsteps.net/" />
-        <meta name="title"  content="Gamal Gomaa Farms - مزارع جمال جمعة
-        تأسست الشركة بهدف إنتاج وتوريد منتجات البساتين الطازجة ذات الجودة الممتازة والآمنة والصحية والتي تتسم بالاستدامة لعملائنا طوال السنة بأسعار تتسم بالتنافسية العالية.
-
-تأسست الشركة بهدف استجابة منا لشغفنا بالتجديد المستمر واحترامنا للبيئة، نراهن على التكنولوجيات الحديثة التي تسهل أن نضع في السوق منتجات تتسم بالجودة الممتازة وتمت تعبئتها تحت إشراف صارم ويتم توريدها" />
-        <meta name="description"
+    <meta name="twitter:url" content="http://farms-website.seniorsteps.net/" />
+    <meta name="twitter:image"
+        content=href="assets/images/resources/232659713_204434118362200_47229516744215480_n_1-1-removebg-preview.png" />
+    <meta property="twitter:image:alt" content="Gamal Gomaa Farms - مزارع جمال جمعة" />
+    <!-- general meta tags  -->
+    <meta name="canonical_tag" content="http://farms-website.seniorsteps.net/" />
+    <meta name="title"
         content="Gamal Gomaa Farms - مزارع جمال جمعة
         تأسست الشركة بهدف إنتاج وتوريد منتجات البساتين الطازجة ذات الجودة الممتازة والآمنة والصحية والتي تتسم بالاستدامة لعملائنا طوال السنة بأسعار تتسم بالتنافسية العالية.
 
 تأسست الشركة بهدف استجابة منا لشغفنا بالتجديد المستمر واحترامنا للبيئة، نراهن على التكنولوجيات الحديثة التي تسهل أن نضع في السوق منتجات تتسم بالجودة الممتازة وتمت تعبئتها تحت إشراف صارم ويتم توريدها" />
-        <meta name="image"  content="Gamal Gomaa Farms - مزارع جمال جمعة
+    <meta name="description"
+        content="Gamal Gomaa Farms - مزارع جمال جمعة
         تأسست الشركة بهدف إنتاج وتوريد منتجات البساتين الطازجة ذات الجودة الممتازة والآمنة والصحية والتي تتسم بالاستدامة لعملائنا طوال السنة بأسعار تتسم بالتنافسية العالية.
 
 تأسست الشركة بهدف استجابة منا لشغفنا بالتجديد المستمر واحترامنا للبيئة، نراهن على التكنولوجيات الحديثة التي تسهل أن نضع في السوق منتجات تتسم بالجودة الممتازة وتمت تعبئتها تحت إشراف صارم ويتم توريدها" />
-        <meta property="image:alt"  content="Gamal Gomaa Farms - مزارع جمال جمعة
+    <meta name="image"
+        content="Gamal Gomaa Farms - مزارع جمال جمعة
         تأسست الشركة بهدف إنتاج وتوريد منتجات البساتين الطازجة ذات الجودة الممتازة والآمنة والصحية والتي تتسم بالاستدامة لعملائنا طوال السنة بأسعار تتسم بالتنافسية العالية.
 
 تأسست الشركة بهدف استجابة منا لشغفنا بالتجديد المستمر واحترامنا للبيئة، نراهن على التكنولوجيات الحديثة التي تسهل أن نضع في السوق منتجات تتسم بالجودة الممتازة وتمت تعبئتها تحت إشراف صارم ويتم توريدها" />
-        <meta name="keywords"
-            content="Farms مزارع جمال خضار  vegetable market سوبر ماركت  farmers union مزارعين بصل potato farm بطاطس مزارع بساتين organic farms near me experience   " />
+    <meta property="image:alt"
+        content="Gamal Gomaa Farms - مزارع جمال جمعة
+        تأسست الشركة بهدف إنتاج وتوريد منتجات البساتين الطازجة ذات الجودة الممتازة والآمنة والصحية والتي تتسم بالاستدامة لعملائنا طوال السنة بأسعار تتسم بالتنافسية العالية.
 
-            <meta name="csrf-token" content="{{ csrf_token() }}">
+تأسست الشركة بهدف استجابة منا لشغفنا بالتجديد المستمر واحترامنا للبيئة، نراهن على التكنولوجيات الحديثة التي تسهل أن نضع في السوق منتجات تتسم بالجودة الممتازة وتمت تعبئتها تحت إشراف صارم ويتم توريدها" />
+    <meta name="keywords"
+        content="Farms مزارع جمال خضار  vegetable market سوبر ماركت  farmers union مزارعين بصل potato farm بطاطس مزارع بساتين organic farms near me experience   " />
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="preconnect" href="https://fonts.googleapis.com/">
 
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
@@ -74,35 +79,11 @@
     <link rel="stylesheet" href="{{asset('webassets/assets/vendors/animate/animate.min.css')}}" />
     <link rel="stylesheet" href="{{asset('webassets/assets/vendors/animate/custom-animate.css')}}" />
     <link rel="stylesheet" href="{{asset('webassets/assets/vendors/fontawesome/css/all.min.css')}}" />
-    {{--
-    <link rel="stylesheet" href="{{asset('webassets/assets/vendors/jarallax/jarallax.css')}}" /> --}}
-    {{--
-    <link rel="stylesheet"
-        href="{{asset('webassets/assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css')}}" /> --}}
-    {{--
-    <link rel="stylesheet" href="{{asset('webassets/assets/vendors/nouislider/nouislider.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('webassets/assets/vendors/nouislider/nouislider.pips.css')}}" /> --}}
-    {{--
-    <link rel="stylesheet" href="{{asset('webassets/assets/vendors/odometer/odometer.min.css')}}" /> --}}
     <link rel="stylesheet" href="{{asset('webassets/assets/vendors/swiper/swiper.min.css')}}" />
     <link rel="stylesheet" href="{{asset('webassets/assets/vendors/ogenix-icons/style.css')}}">
-    {{--
-    <link rel="stylesheet" href="{{asset('webassets/assets/vendors/tiny-slider/tiny-slider.min.css')}}" /> --}}
     <link rel="stylesheet" href="{{asset('webassets/assets/vendors/reey-font/stylesheet.css')}}" />
     <link rel="stylesheet" href="{{asset('webassets/assets/vendors/owl-carousel/owl.carousel.min.css')}}" />
     <link rel="stylesheet" href="{{asset('webassets/assets/vendors/owl-carousel/owl.theme.default.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('webassets/assets/vendors/bxslider/jquery.bxslider.css')}}" />
-    {{--
-    <link rel="stylesheet" href="{{asset('webassets/assets/vendors/bootstrap-select/css/bootstrap-select.min.css')}}" />
-    --}}
-    {{--
-    <link rel="stylesheet" href="{{asset('webassets/assets/vendors/vegas/vegas.min.css')}}" /> --}}
-    <link rel="stylesheet" href="{{asset('webassets/assets/vendors/jquery-ui/jquery-ui.css')}}" />
-    <link rel="stylesheet" href="{{asset('webassets/assets/vendors/timepicker/timePicker.css')}}" />
-    {{--
-    <link rel="stylesheet" href="{{asset('webassets/assets/vendors/nice-select/nice-select.css')}}" /> --}}
-
-
     <link rel="stylesheet" href="{{asset('webassets/assets/css/mainstyle.min.css')}}" />
     <link rel="stylesheet" href="{{asset('webassets/assets/css/mainstyle-responsive.min.css')}}" />
 </head>
@@ -130,32 +111,40 @@
                     style="background-image: url({{asset('webassets/assets/images/shapes/main-slider-bg-shape-two.png')}});">
                 </div>
                 <div class="main-slider__shape-1 float-bob-y">
-                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-1.png')}}" alt="main-slider-shape-1.png">
+                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-1.png')}}"
+                        alt="main-slider-shape-1.png">
                 </div>
                 <div class="main-slider__shape-2 float-bob-x">
-                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-2.png')}}" alt="main-slider-shape-2.png">
+                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-2.png')}}"
+                        alt="main-slider-shape-2.png">
                 </div>
                 <div class="main-slider__shape-3 float-bob-x">
-                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-3.png')}}" alt="main-slider-shape-3.png">
+                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-3.png')}}"
+                        alt="main-slider-shape-3.png">
                 </div>
                 <div class="main-slider__shape-4 float-bob-y">
-                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-4.png')}}" alt="main-slider-shape-4.png">
+                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-4.png')}}"
+                        alt="main-slider-shape-4.png">
                 </div>
                 <div class="main-slider__shape-5 zoominout">
-                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-5.png')}}" alt="main-slider-shape-5.png">
+                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-5.png')}}"
+                        alt="main-slider-shape-5.png">
                 </div>
                 <div class="main-slider__shape-6 float-bob-x">
-                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-6.png')}}" alt="main-slider-shape-6.png">
+                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-6.png')}}"
+                        alt="main-slider-shape-6.png">
                 </div>
                 <div class="main-slider__shape-7 float-bob-y">
-                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-7.png')}}" alt="main-slider-shape-7.png">
+                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-7.png')}}"
+                        alt="main-slider-shape-7.png">
                 </div>
                 <div class="main-slider__shape-8 float-bob-x">
-                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-8.png')}}" alt="main-slider-shape-8.png">
+                    <img src="{{asset('webassets/assets/images/shapes/main-slider-shape-8.png')}}"
+                        alt="main-slider-shape-8.png">
                 </div>
                 <div class="main-slider__img-one">
-                    <img src="{{asset('webassets/assets/images/resources/main-slider-img-1.webp')}}" alt="main-slider-img-1.webp"
-                        class="img-bounce">
+                    <img src="{{asset('webassets/assets/images/resources/main-slider-img-1.webp')}}"
+                        alt="main-slider-img-1.webp" class="img-bounce">
                 </div>
                 <div class="container">
                     <div class="row">
@@ -560,29 +549,6 @@
     integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous">
 </script>
 <script async src="{{asset('webassets/assets/vendors/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/jarallax/jarallax.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/jquery-appear/jquery.appear.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/jquery-validate/jquery.validate.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/nouislider/nouislider.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/odometer/odometer.min.js')}}"></script>
 <script async src="{{asset('webassets/assets/vendors/swiper/swiper.min.js')}}"></script>
-
-<script async src="{{asset('webassets/assets/vendors/tiny-slider/tiny-slider.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/wnumb/wNumb.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/wow/wow.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/isotope/isotope.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/countdown/jquery.countdown.min.js')}}"></script>
 <script async src="{{asset('webassets/assets/vendors/owl-carousel/owl.carousel.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/bxslider/jquery.bxslider.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/bootstrap-select/js/bootstrap-select.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/vegas/vegas.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/jquery-ui/jquery-ui.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/timepicker/timePicker.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/circleType/jquery.circleType.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/circleType/jquery.lettering.min.js')}}"></script>
-<script async src="{{asset('webassets/assets/vendors/nice-select/jquery.nice-select.min.js')}}"></script>
-
 <script async src="{{asset('webassets/assets/js/mainjs.js')}}"></script>
