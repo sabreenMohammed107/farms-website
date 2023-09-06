4ca0eea649ee6501aed5a992dac3b1f76117926d
sabreenMohammed107 <sabreenm312@gmail.com>
2023-09-06 23:21:47 +0200

    fav meta

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,7 @@
 
 <head>
     <title>مزارع جمال جمعة - منتجات زراعية</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('img/logo.PNG') }}">
-
+    <link rel="shortcut icon" href="{{ asset('img/logo.PNG') }}" type="image/x-icon">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
