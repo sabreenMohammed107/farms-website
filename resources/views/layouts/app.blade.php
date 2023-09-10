35f71a85f109597100bcb21c5501f35eb49b35f5
sabreenMohammed107 <sabreenm312@gmail.com>
2023-09-10 10:13:04 +0200

    reset before ahmed edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <link rel="shortcut icon" href="{{ asset('img/logo.webp') }}" />
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
