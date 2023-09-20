35bfcc8f339f9b4e374e94007a88e822533cb13d
sabreenMohammed107 <sabreenm312@gmail.com>
2023-09-20 20:33:57 +0200

    testing

--- resources/views/layout/webLayout/main.blade.php
+++ resources/views/layout/webLayout/main.blade.php
@@ -2,7 +2,20 @@
 
 
 <!--end::Head-->
-
+<style>
+    .page-header-bg:before {
+    content: "";
+    top: 0;
+    background: linear-gradient( 0deg, rgb(0 0 0 / 56%) 0, rgba(0, 0, 0, 0.24) 100% );
+}
+.page-header__inner h3,h1{
+    color:#fff;
+}
+.thm-breadcrumb li a:hover {
+color: #fff;
+text-decoration: underline
+}
+    </style>
 <body class="custom-cursor">
 
     <div class="custom-cursor__cursor"></div>
